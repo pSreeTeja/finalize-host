4485b7882c46e781e8f4cf2666a9fe4c89e9c5ba
Sree Teja <psteja02@gmail.com>
2022-02-01 16:39:30 +0530

    added isAuthenticatedApi

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,18 +2,17 @@ const jwt = require("jsonwebtoken");
 const UserModel = require("../models/user");
 
 const Authenticate = async (req, res, next) => {
-  console.log("Hello from authenticate");
   try {
     const token = req.cookies.jwtoken;
     const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
     const rootUser = await UserModel.findOne({ _id: verifyToken._id });
     console.log(rootUser);
     if (!rootUser) {
-      console.log("throwing error!!!!");
       throw new Error("User not found");
     }
     req.token = token;
     req.rootUser = rootUser;
+    req.status = 200;
     next();
   } catch (err) {
     res.status(401).send("unauthorized");
