135c09962c4e6f2805d13747276f17c997f72035
Sree Teja <psteja02@gmail.com>
2022-12-04 13:27:33 +0530

    modified headers

--- index.js
+++ index.js
@@ -15,9 +15,13 @@ require("dotenv").config();
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.header(
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
   next();
 });
